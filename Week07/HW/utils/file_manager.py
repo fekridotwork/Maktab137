b8247559eb3c7487f72065a7d27b4916a5c876da
Matin Fekri <fekridotwork@gmail.com>
2025-10-25 18:05:46 +0330

    file_manager implemented

---
+++ Week07/HW/utils/file_manager.py
@@ -0,0 +1,13 @@
+import json
+
+def load_data(file_path):
+    try:
+        with open(file_path, mode='r', encoding='utf-8') as file:
+            data = json.load(file)
+            return data
+    except (FileNotFoundError, json.JSONDecodeError): # It's better to separate
+        return []
+
+def save_data(file_path, data):
+    with open(file_path, mode='r', encoding='utf-8') as file:
+        json.dump(data, file, indent=4)
