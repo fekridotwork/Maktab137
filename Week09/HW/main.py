977e1831402432e5d5844683fb5389173494f588
Matin Fekri <fekridotwork@gmail.com>
2025-11-10 11:12:29 +0330

    First step and implementation.

---
+++ Week09/HW/main.py
@@ -0,0 +1,14 @@
+while True:
+    choice = input("How do you want to Login ?\n1.Email-Password\n2.OTP\n3.Google Login\nYour choice : ")
+    if choice == "1":
+        # Email- Password Login
+        break
+    elif choice == "2":
+        # OTP Login
+        break
+    elif choice == "3":
+        # Google Login
+        break
+    else:
+        print("Invalid Choice! --> Try Again.")
+        continue
